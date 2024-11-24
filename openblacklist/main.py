05873e546323919c8774004cc837e7e9c31b1c9b
Alexis <awaknocode@gmail.com>
2024-11-24 14:32:17 +0100

    Add webhook password support and refactor webhook handling methods

--- openblacklist/main.py
+++ openblacklist/main.py
@@ -5,7 +5,7 @@ from typing import Optional, Dict, List, Callable
 from .method.Model import UserBlacklist, User, Reason, UserBlacklistWebhook
 
 class OpenBlacklistClient:
-    def __init__(self, api_key: str, url='https://openbl.clarty.org/api/v1/', webhook_url: Optional[str] = None):
+    def __init__(self, api_key: str=None, url='https://openbl.clarty.org/api/v1/', webhook_url: Optional[str] = None,webhook_pass: str=None):
         """
         Initializes the OpenBlacklistClient.
 
@@ -17,11 +17,12 @@ class OpenBlacklistClient:
         self.url = url
         self.api_key = api_key
         self.webhook_url = webhook_url
+        self.webhook_pass = webhook_pass
         self.event_handlers: Dict[str, List[Callable]] = {}
         self.app = FastAPI()
 
         if webhook_url:
-            self.setup_webhook_endpoint()
+            self._setup_webhook_endpoint()
 
     async def check_user(self, user_id: int) -> UserBlacklist:
         """
@@ -33,6 +34,8 @@ class OpenBlacklistClient:
         Returns:
             UserBlacklist: The blacklist information of the user.
         """
+        if not self.api_key:
+            raise ValueError("API key is required to check user blacklist status")
         async with aiohttp.ClientSession(raise_for_status=True) as session:
             async with session.get(
                 self.url + f"/user/{user_id}", headers={"Authorization": self.api_key}
@@ -42,7 +45,7 @@ class OpenBlacklistClient:
                     return UserBlacklist(**data)
                 else:
                     return UserBlacklist(isBlacklisted=False, user=User(id=user_id, username="",blacklisted_reasons=Reason(fr_fr="", en_gb="", es_sp="")))
-    async def handle_webhook(self, data: dict):
+    async def _handle_webhook(self, data: dict):
         """
         Handles data received via webhook and triggers specific events.
 
@@ -55,6 +58,8 @@ class OpenBlacklistClient:
         user = User(**data['user'])
         reason = Reason(**data['reasons'])
         metadata = data['metadata']
+        if metadata.get("pass") != self.webhook_pass:
+            return
         webhook_event = UserBlacklistWebhook(user=user, reason=reason, metadata=metadata)
 
         event_name = metadata.get("event")
@@ -64,7 +69,7 @@ class OpenBlacklistClient:
 
         return webhook_event
 
-    def event(self, event_name: str):
+    def _event(self, event_name: str):
         """
         Decorator to register an event handler.
 
@@ -81,13 +86,13 @@ class OpenBlacklistClient:
             return func
         return decorator
 
-    def setup_webhook_endpoint(self):
+    def _setup_webhook_endpoint(self):
         """
         Adds a webhook endpoint to the FastAPI application.
         """
         @self.app.post(f"/{self.webhook_url}")
         async def webhook_listener(data: dict = Body(...)):
-            await self.handle_webhook(data)
+            await self._handle_webhook(data)
             return {"status": "success"}
 
     def listen(self, host: str = "0.0.0.0", port: int = 5000):
